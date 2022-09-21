4c7b83d7419fae7fe4405665e81bed2e66a453aa
Xavier Chopin <xchopin54@gmail.com>
2022-09-21 18:50:49 -0400

    fix: wrong info when impound take out

--- client/client.lua
+++ client/client.lua
@@ -16,11 +16,9 @@ end
 
 RegisterNUICallback("npwd:qb-garage:getVehicles", function(_, cb)
 	TriggerServerEvent("npwd:qb-garage:getVehicles")
-
 	RegisterNetEvent("npwd:qb-garage:sendVehicles", function(vehicles)
 		for _, v in pairs(vehicles) do
-			local vehicleModel = v.vehicle
-			local type = GetVehicleClassFromName(vehicleModel)
+			local type = GetVehicleClassFromName(v.model)
 			if type == 15 or type == 16 then
 				v.type = "aircraft"
 			elseif type == 14 then
@@ -29,22 +27,6 @@ RegisterNUICallback("npwd:qb-garage:getVehicles", function(_, cb)
 				v.type = "bike"
 			else
 				v.type = "car"
-			end
-			if v.state == 0 then
-				v.state = "out"
-			elseif v.state == 1 then
-				v.state = "garaged"
-			elseif v.state == 2 then
-				v.state = "impounded"
-			-- elseif v.state == 3 then -- add new state for seized vehicles
-			-- 	v.state = "seized"
-			else 
-				v.state = "unknown"
-			end
-			if (QBCore.Shared.Vehicles[vehicleModel] ~= nil) and (Garages[v.garage] ~= nil) then
-				v.garage = Garages[v.garage].label
-				v.vehicle = QBCore.Shared.Vehicles[vehicleModel].name
-				v.brand = QBCore.Shared.Vehicles[vehicleModel].brand
 			end
 		end
 
--- server/server.lua
+++ server/server.lua
@@ -3,6 +3,39 @@ local QBCore = exports['qb-core']:GetCoreObject()
 RegisterNetEvent("npwd:qb-garage:getVehicles", function()
 	local src = source
 	local Player  = QBCore.Functions.GetPlayer(src)
-	local garageresult = MySQL.query.await('SELECT vehicle, plate, garage, fuel, engine, body, state, hash FROM player_vehicles WHERE citizenid = ?', {Player.PlayerData.citizenid})
-	TriggerClientEvent('npwd:qb-garage:sendVehicles', src, garageresult)
+	local garageresult = MySQL.query.await('SELECT * FROM player_vehicles WHERE citizenid = ?', {Player.PlayerData.citizenid})
+
+	if garageresult[1] ~= nil then
+		for _, v in pairs(garageresult) do
+			local vehicleModel = v.vehicle
+			v.model = vehicleModel
+			v.vehicle = 'Unknown'
+			v.brand = 'Vehicle'
+
+			if v.state == 0 then
+				v.state = "out"
+			elseif v.state == 1 then
+				v.state = "garaged"
+			elseif v.state == 2 then
+				v.state = "impounded"
+				-- elseif v.state == 3 then -- add new state for seized vehicles
+				-- 	v.state = "seized"
+			else
+				v.state = "unknown"
+			end
+
+			if QBCore.Shared.Vehicles[vehicleModel] then
+				v.vehicle = QBCore.Shared.Vehicles[vehicleModel].name
+				v.brand = QBCore.Shared.Vehicles[vehicleModel].brand
+			end
+
+			if (Garages[v.garage] ~= nil) then
+				v.garage = Garages[v.garage].label
+			else
+				v.garage = "Unknown Garage"
+			end
+		end
+
+		TriggerClientEvent('npwd:qb-garage:sendVehicles', src, garageresult)
+	end
 end)
